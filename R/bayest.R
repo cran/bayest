c93d867f8d86f5ca9102ac85f71975a1415ee86a
Riko Kelter <riko.kelter@uni-siegen.de>
2020-05-27 07:10:02 +0000

    version 1.3

--- R/bayest.R
+++ R/bayest.R
@@ -20,7 +20,7 @@ bayes.t.test <-
            firstComp,
            secondComp,
            hyperpars = "wide",
-           ci = "0.95",
+           ci = 0.95,
            burnin = n / 2,
            sd = "sd",
            q = 0.1) {
@@ -908,5 +908,8 @@ bayes.t.test <-
                       UpperCI,
                       MAPE)
       df
+    }
+    if(plot == "samples"){
+      effectSize
     }
   }
